9016c73e8a94b666c7da5fd9e308ee45d1924b67
Azeez Adebisi <samadebisi12@gmail.com>
2025-07-02 03:05:19 +0300

    Update auto_grad.py

--- 3-way classification/auto_grad.py
+++ 3-way classification/auto_grad.py
@@ -1,3 +1,4 @@
 """
-Defines a simple autograd engine and uses it to classify points in the plane to 3 classes,     
+Defines a simple autograd engine and uses it to classify points in the plane to 3 classes  
+(red, green, blue) using a simple multilayer perceptron (MLP).   
 """
