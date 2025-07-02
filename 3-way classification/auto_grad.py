4e11c7cf58d0b1ab36f8fe398b278e6d2025a592
Azeez Adebisi <samadebisi12@gmail.com>
2025-07-03 00:50:35 +0300

    Update auto_grad.py

--- 3-way classification/auto_grad.py
+++ 3-way classification/auto_grad.py
@@ -34,3 +34,7 @@ class Value:
     
     def __mul__(self, other):
         other = other if isinstance(other, Value) else Value(other)
+        out = Value(self.data * other.data, (self, other), '*')
+        
+        def _backward():
+            self.grad += other.data * out.grad 
