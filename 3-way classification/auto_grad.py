306ae6d81dba24d27efbc8517a8d48266811e0cf
Azeez Adebisi <samadebisi12@gmail.com>
2025-07-03 00:15:38 +0300

    Update auto_grad.py

--- 3-way classification/auto_grad.py
+++ 3-way classification/auto_grad.py
@@ -6,3 +6,5 @@ Defines a simple autograd engine and uses it to classify points in the plane to 
 import math 
 from utils import RNG, gen_data_yinyang, draw_dot, vis_color 
 random = RNG(42) 
+
+# ---------------------------------------------
