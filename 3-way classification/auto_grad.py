13480dca5f508cf8f122a4ca97746649a6f22889
Azeez Adebisi <samadebisi12@gmail.com>
2025-07-04 02:16:46 +0300

    Update auto_grad.py

--- 3-way classification/auto_grad.py
+++ 3-way classification/auto_grad.py
@@ -55,3 +55,7 @@ class Value:
     
     def relu(self):
         out = Value(0 if self.data < 0 else self.data, (self,), 'ReLU')
+        
+        def _backward():
+            self.grad += (out.data > 0) * out.grad 
+        out._backward = _backward 
